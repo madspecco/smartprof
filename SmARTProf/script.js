22f151a1328905bec15b9b0d816c29fe2b6b63bd
madspecco <bogdan.bolos30@gmail.com>
2024-10-17 19:21:07 +0300

    Added skeleton for pages
    - register page
    - language selection page

--- SmARTProf/script.js
+++ SmARTProf/script.js
@@ -1,11 +1,28 @@
 function login() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    // login logic here
-    alert(`Logging in with Username: ${username}`);
+    // validation of login
+    // if successful go to select-language page
+    window.location.href = "select-language.html";
 }
 
-function createAccount() {
-    // account creation page
-    alert('Redirecting to account creation...');
+function goToRegister() {
+    window.location.href = "register.html";
+}
+
+function register() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match!");
+        return;
+    }
+
+    // registration logic here
+    window.location.href = "select-language.html";
+}
+
+function selectLanguage(language) {
+    alert(`You selected ${language}!`);
+    // Add logic to proceed with the selected language
 }
