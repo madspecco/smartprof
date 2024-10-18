46294494012bd2975bb419d852e9eb81fb27e4aa
madspecco <bogdan.bolos30@gmail.com>
2024-10-18 10:31:51 +0300

    Changed model

--- SmARTProf/config.py
+++ SmARTProf/config.py
@@ -4,18 +4,18 @@ index_dict = {
     1: ("envelope", "busta", "sobre", "plic", "enveloppe"),
     2: ("eye", "occhio", "ojo", "ochi", "œil"),
     3: ("parachute", "paracadute", "paracaídas", "parașută", "parachute"),
-    4: ("television", "televisione", "televisión", "televiziune", "télévision"),
-    5: ("tent", "tenda", "tienda", "cort", "tente"),
-    6: ("shorts", "pantaloncini", "pantalones cortos", "șorturi", "shorts"),
-    7: ("snake", "serpente", "serpiente", "șarpe", "serpent"),
-    8: ("snowflake", "fiocco di neve", "copo de nieve", "fulg de zăpadă", "flocon de neige"),
-    9: ("ice cream", "gelato", "helado", "înghețată", "crème glacée"),
-    10: ("bee", "ape", "abeja", "albină", "abeille"),
-    11: ("leaf", "foglia", "hoja", "frunză", "feuille"),
-    12: ("guitar", "chitarra", "guitarra", "chitară", "guitare"),
-    13: ("key", "chiave", "clave", "cheie", "clé"),
-    14: ("knife", "coltello", "cuchillo", "cuțit", "couteau"),
-    15: ("lighter", "accendino", "encendedor", "brichetă", "briquet")
+    4: ("television", "televisione", "televisión", "televizor", "télévision"),
+    # 5: ("tent", "tenda", "tienda", "cort", "tente"),
+    # 6: ("shorts", "pantaloncini", "pantalones cortos", "șorturi", "shorts"),
+    # 7: ("snake", "serpente", "serpiente", "șarpe", "serpent"),
+    # 8: ("snowflake", "fiocco di neve", "copo de nieve", "fulg de zăpadă", "flocon de neige"),
+    # 9: ("ice cream", "gelato", "helado", "înghețată", "crème glacée"),
+    # 10: ("bee", "ape", "abeja", "albină", "abeille"),
+    # 11: ("leaf", "foglia", "hoja", "frunză", "feuille"),
+    # 12: ("guitar", "chitarra", "guitarra", "chitară", "guitare"),
+    # 13: ("key", "chiave", "clave", "cheie", "clé"),
+    # 14: ("knife", "coltello", "cuchillo", "cuțit", "couteau"),
+    # 15: ("lighter", "accendino", "encendedor", "brichetă", "briquet")
 }
 
 PATH_TO_DRAWING = "C://repos//smartprof//SmARTProf//drawing.png"
